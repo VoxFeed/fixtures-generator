afee9ccd799d89ee6d776476a7861b155388665e
Gibran Arias <gibranarias@gmail.com>
2018-09-26 13:24:10 -0500

    Create Generator class

--- generate.js
+++ generate.js
@@ -1,57 +1,52 @@
 const {isArray, isInteger, mergeWith} = require('lodash');
-const schemas = require('schemas');
+const schemas = require('vf-schemas');
 const schemaFaker = require('./schema-faker');
 const validateFixture = require('./validate');
 const replaceFormats = require('./replace-formats');
 
-module.exports = (schemas) => {
-  if (!schemas) throw new Error('schemas was not defined.')
-  const generateFixtures = (params) => {
-    const {type} = params;
-    const schema = schemas[type];
+const generateFixtures = (schemas, params) => {
+  const {type} = params;
+  const schema = schemas[type];
 
-    validateParams(params);
-    if (!schema) throw new Error(`Fixtures schema not found for type: ${type}`);
+  validateParams(params);
+  if (!schema) throw new Error(`Fixtures schema not found for type: ${type}`);
 
-    const recipe = getRecipes(params);
-    return recipe.map(r => generateFixture(schema, r));
-  };
+  const recipe = getRecipes(params);
+  return recipe.map(r => generateFixture(schema, r));
+};
 
-  const generateFixture = (schema, recipe) => {
-    const raw = schemaFaker(schema);
-    const base = replaceFormats(raw);
+const generateFixture = (schema, recipe) => {
+  const raw = schemaFaker(schema);
+  const base = replaceFormats(raw);
 
-    const fixture = mergeWith({}, base, recipe, (baseValue, recipeValue, key, object) => {
-      if (isArray(recipeValue)) return recipeValue;
-      if (recipeValue === undefined) object[key] = undefined;
-    });
+  const fixture = mergeWith({}, base, recipe, (baseValue, recipeValue, key, object) => {
+    if (isArray(recipeValue)) return recipeValue;
+    if (recipeValue === undefined) object[key] = undefined;
+  });
 
-    return validateFixture(schema, fixture);
-  };
+  return validateFixture(schema, fixture);
+};
 
-  const isPositive = num => isInteger(num) && num >= 0;
+const isPositive = num => isInteger(num) && num >= 0;
 
-  const validateParams = (params) => {
-    const {recipe, quantity} = params;
-    if (!recipe && !quantity) throw new Error(`At least one parameter ("recipe" or "quantity") is required. Received: ${params}`);
-    if (recipe && !isArray(recipe)) throw new Error(`"recipe" must be an array. Received: ${recipe}`);
-    if (quantity && !isPositive(quantity)) throw new Error(`"quantity" must be a positive integer. Received: ${quantity}`);
-  };
+const validateParams = (params) => {
+  const {recipe, quantity} = params;
+  if (!recipe && !quantity) throw new Error(`At least one parameter ("recipe" or "quantity") is required. Received: ${params}`);
+  if (recipe && !isArray(recipe)) throw new Error(`"recipe" must be an array. Received: ${recipe}`);
+  if (quantity && !isPositive(quantity)) throw new Error(`"quantity" must be a positive integer. Received: ${quantity}`);
+};
 
-  const fill = (array, fillWith, start, end) => {
-    for (let i = start; i < end; i++) array[i] = {};
-    return array;
-  };
+const fill = (array, fillWith, start, end) => {
+  for (let i = start; i < end; i++) array[i] = {};
+  return array;
+};
 
-  const getRecipes = (params) => {
-    const quantity = params.quantity || 0;
-    const recipe = params.recipe || [];
-    const initalLength = recipe.length;
-    const actualLength = Math.max(quantity, initalLength);
+const getRecipes = (params) => {
+  const quantity = params.quantity || 0;
+  const recipe = params.recipe || [];
+  const initalLength = recipe.length;
+  const actualLength = Math.max(quantity, initalLength);
 
-    return fill(recipe, {}, initalLength, actualLength);
-  };
-
-  return {generateFixtures}
-}
-// module.exports = generateFixtures;
+  return fill(recipe, {}, initalLength, actualLength);
+};
+module.exports = generateFixtures;
--- test/small/build-generator.js
+++ test/small/build-generator.js
@@ -1,7 +1,6 @@
-const buildGenerator = require('../../generate.js');
 const chai = require('chai');
 const expect = chai.expect;
-
+const Generator = require('../../index')
 describe('Build generator instance', () => {
   const schemas = {
     user: {
@@ -27,57 +26,57 @@ describe('Build generator instance', () => {
   };
 
   it('should notify that schemas are required', () => {    
-    expect(() => buildGenerator()).to.throw('schemas was not defined.');
+    expect(() => new Generator()).to.throw('schemas was not defined.');
   });
 
   it('should notify that fixture schema is not found', () => {
-    const generator = buildGenerator([]);
-    expect(() => generator.generateFixtures({
+    const generator = new Generator(schemas);
+    expect(() => generator.generate({
       type: 'bikes', recipe: ['schemas']
     })).to.throw('Fixtures schema not found for type: bikes');
   });
 
   it('should notify that at least one recipe or quantity is required', () => {    
-    const generator = buildGenerator(schemas);
-    expect(() => generator.generateFixtures({
+    const generator = new Generator(schemas);
+    expect(() => generator.generate({
       type: 'brand'
     })).to.throw('At least one parameter ("recipe" or "quantity") is required.');
   });
 
   it('should notify that "recipe must be an array"', () => {    
-    const generator = buildGenerator(schemas);
-    expect(() => generator.generateFixtures({
+    const generator = new Generator(schemas);
+    expect(() => generator.generate({
       type: 'brand', recipe: 'test'
     })).to.throw('"recipe" must be an array. Received: test');
   });
 
   it('should notify that "quantity" must be positive integer', () => {    
-    const generator = buildGenerator(schemas);
-    expect(() => generator.generateFixtures({
+    const generator = new Generator(schemas);
+    expect(() => generator.generate({
       type: 'brand', quantity: 'test'
     })).to.throw('"quantity" must be a positive integer. Received: test');
   });
 
   it('should get correct schema in the result', () => {   
     const userRecipe = [{ name: 'test brand', gender: 'male'}];
-    const generator = buildGenerator(schemas);
-    expect(() => generator.generateFixtures({
+    const generator = new Generator(schemas);
+    expect(() => generator.generate({
       type: 'brand', recipe: userRecipe
     })).to.throw('Validation schema failed for fixture');
   });
 
   it('should get correct schema in the result', () => {   
     const userRecipe = [{ name: 'test brand', gender: 'male'}];
-    const generator = buildGenerator(schemas);
-    const result = generator.generateFixtures({ type: 'user', recipe: userRecipe });
+    const generator = new Generator(schemas);
+    const result = generator.generate({ type: 'user', recipe: userRecipe });
     expect(result[0].name).to.be.equal(userRecipe[0].name);
     expect(result[0].gender).to.be.equal(userRecipe[0].gender);
   });
 
   it('should get correct quantity of objects', () => {
-    quantityToGenerate = 3
-    const generator = buildGenerator(schemas);
-    const result = generator.generateFixtures({ type: 'user', quantity: quantityToGenerate });
+    const quantityToGenerate = 3
+    const generator = new Generator(schemas);
+    const result = generator.generate({ type: 'user', quantity: quantityToGenerate });
     expect(result.length).to.be.equal(quantityToGenerate);
   });
 });
